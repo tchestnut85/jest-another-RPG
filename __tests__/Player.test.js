71089f843691faac840212932d82a93820629856
Tom Chestnut <tchestnut85@gmail.com>
2020-10-05 21:38:52 -0400

    added funtions and test for player stats and inventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,6 +1,5 @@
 const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
-console.log(new Potion());
 
 const Player = require('../lib/Player');
 
@@ -14,4 +13,23 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Frodo');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Frodo');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
