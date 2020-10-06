e8f4416063b9ccd7c8a862b1d5f0f313e64318c1
Tom Chestnut <tchestnut85@gmail.com>
2020-10-06 18:33:52 -0400

    added final functions and tests for the player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -62,3 +62,30 @@ test("subtracts from player's health", () => {
 
     expect(player.health).toBe(0);
 });
+
+test('get player attack value', () => {
+    const player = new Player('Frodo');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds a potion to the inventory', () => {
+    const player = new Player('Frodo');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('uses a potion from inventory', () => {
+    const player = new Player('Frodo');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
